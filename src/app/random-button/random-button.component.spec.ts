65d87ebe4fac7aacbd6b00b235e51a68c2ee7a39
Sean <sjgallen@gmail.com>
2022-12-29 23:07:55 -0600

    randomButton

---
+++ src/app/random-button/random-button.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RandomButtonComponent } from './random-button.component';
+
+describe('RandomButtonComponent', () => {
+  let component: RandomButtonComponent;
+  let fixture: ComponentFixture<RandomButtonComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RandomButtonComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(RandomButtonComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/random-button/random-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-random-button',
+  templateUrl: './random-button.component.html',
+  styleUrls: ['./random-button.component.css']
+})
+export class RandomButtonComponent {
+
+}
